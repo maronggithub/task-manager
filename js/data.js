5a4e9732ac056dd40c6b6dda8d5b8065f3d20ade
Jinghua Li <jinhli@thoughtworks.com>
2020-07-29 22:21:01 +0800

    Remove useless files

--- js/data.js
+++ js/data.js
@@ -5,20 +5,3 @@ function saveAllTasks(allTasks) {
 function getAllTasks() {
   return JSON.parse(localStorage.getItem('tasks'));
 }
-
-function removeTask(id) {
-  let leftTasks = getAllTasks().filter((task) => task.id !== id);
-  saveAllTasks(leftTasks);
-}
-
-function findTask(id) {
-  return getAllTasks().find((task) => task.id === id);
-}
-
-function findTasksByName(name) {
-  return getAllTasks().filter((task) => task.name.includes(name));
-}
-
-function findTasksByStatus(status) {
-  return getAllTasks().filter((task) => task.status === status);
-}
